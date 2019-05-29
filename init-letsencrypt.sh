5732e395b92b7fed6f3b65d5cd4fb123890bcacf
si9ma <hellob374@gmail.com>
2019-05-29 12:03:39 +0800

    update config

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,7 +4,7 @@ domains=(si9ma.com wiki-store.si9ma.com oj.si9ma.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="si9ma@si9ma.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
